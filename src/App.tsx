a68b421e486b4fb08a04354bb3d8b5074abd7aa2
KilianSen <kilian@senger.group>
2025-12-22 19:18:46 +0100

    Update TypeScript configuration to target ESNext and improve logging in App component

--- src/App.tsx
+++ src/App.tsx
@@ -191,6 +191,7 @@ function App() {
                                                         {
                                                             // If any containers are either 'unhealthy' or 'exited' or 'dead' or 'unknown', show a red badge with the count of such containers
                                                             (() => {
+                                                                console.log(containers);
                                                                 const problemContainers = (containers || []).filter(c => ['debug', 'unhealthy', 'exited', 'dead', 'unknown'].includes(c.status));
                                                                 if (problemContainers.length > 0) {
                                                                     return <Badge variant="destructive">{problemContainers.length} issue{problemContainers.length > 1 ? 's' : ''}</Badge>;
